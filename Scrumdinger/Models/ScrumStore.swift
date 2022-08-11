5a0442e54c77721f9e256e819bb6439e39f04ef2
sundaycrafts <sundaycrafts@users.noreply.github.com>
2022-08-11 16:38:21 +0200

    Modernizing Asynchronous Code - Persistence and Concurrency

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -16,6 +16,19 @@ class ScrumStore: ObservableObject {
                 .appendingPathComponent("scrums.data")
     }
 
+    static func load() async throws -> [DailyScrum] {
+        try await withCheckedThrowingContinuation { continuation in
+            load { result in
+                switch result {
+                case .success(let scrums):
+                    continuation.resume(returning: scrums)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
+                }
+            }
+        }
+    }
+
     static func load(completion: @escaping (Result<[DailyScrum], Error>) -> Void) {
         DispatchQueue.global(qos: .background).async {
             do {
@@ -35,6 +48,20 @@ class ScrumStore: ObservableObject {
             } catch {
                 DispatchQueue.main.async {
                     completion(.failure(error))
+                }
+            }
+        }
+    }
+
+    @discardableResult
+    static func save(scrums: [DailyScrum]) async throws -> Int {
+        try await withCheckedThrowingContinuation { continuation in
+            save(scrums: scrums) { result in
+                switch result {
+                case .success(let scrumsSaved):
+                    continuation.resume(returning: scrumsSaved)
+                case .failure(let error):
+                    continuation.resume(throwing: error)
                 }
             }
         }
